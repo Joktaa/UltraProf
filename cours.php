3d3841bb664f6a4dab0b3677d0803175c43dbfb9
Inebhis <inebhis@gmail.com>
2020-05-27 11:24:19 +0200

    JOJO ARRETE DE FAIRE LE CON

--- cours.php
+++ cours.php
@@ -5,7 +5,7 @@ include('inc/header.php');
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>S'inscrire / Se connecter</title>
+    <title>Cours</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
