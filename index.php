aa384144bd02c5f50795211d1894bffe02020638
Joktaa <joris.rouziere@gmail.com>
2020-05-28 12:48:35 +0200

    index

--- index.php
+++ index.php
@@ -11,9 +11,8 @@ include('inc/header.php');
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/2.5.0/css/bootstrap.min.css" integrity="sha282-9aIt2nRpC12Uk9gS9baDl211NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <!-- CSS -->
     <style>
-        label{margin-top: 10px; margin-bottom: 0px;}
-        button{margin-top: 15px;}
-        .box{width: 500px; border: 1px solid #ced2da; border-radius: 2px; margin-top: 50px;}
+        .marge-top-bottom{margin: 15px 0px;}
+        .marge-all{margin: 15px;}
     </style>
   </head>
 <body>
@@ -21,7 +20,7 @@ include('inc/header.php');
         <div class="container">
             <h2 style="font-size: 20px">Apprendre c'est bien,<br>Apprendre des meilleurs c'est mieux.</h2>
             <form class="form-inline" action="/action_page.php">
-                <input class="form-control mr-sm-2" type="text" placeholder="Que voulez-vous apprendre ?">
+                <input class="form-control mr-sm-2" type="text" style="width: 50%;" placeholder="Que voulez-vous apprendre ?">
                 <button class="btn btn-success" type="submit">Search</button>
             </form>
             <h2 style="font-size: 20px">Nos profs vous aident à devenir meilleur.</h2>
@@ -32,7 +31,7 @@ include('inc/header.php');
     <div class="container" style="border: 1px solid black">
         <!-- FAIRE UNE BOUCLE POUR AFFICHER TOUT LES PROFILS -->
         <div class="row">
-            <div class="col-4">
+            <div class="col-4 marge-top-bottom">
                 <div class="card text-left">
                     <img class="card-img-top" src="./medecin.png">
                     <div class="card-body">
@@ -47,13 +46,49 @@ include('inc/header.php');
         </div>
     </div>
 
-    <footer style="background-color: #e9ecef;">
+    <footer style="background-color: #e9ecef; margin-top: 20px;">
         <div class="container">
             <div class="row">
-                <div class="col-4" style="border: 1px solid silver">
+                <div class="col-3 marge-top-bottom mx-auto" style="border: 1px solid silver">
                     <h5 class="text-center text-primary" style="font-size: 15px">Comment ça marche ?</h5>
+                    <p style="color: grey;">Vous êtes bloqués dans votre progression ? Les profs d'ultraprof se mettent à votre service pour vous venir en aide.</p>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-3 marge-top-bottom mx-auto" style="border: 1px solid silver">
+                    <h5 class="text-center text-primary" style="font-size: 15px">Devenir prof</h5>
+                    <p style="color: grey;">Partagez votre expérience et votre passion en gagnant de l'argent sur ultraprof. Postulez pour devenir prof en ligne !</p>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-3 marge-top-bottom mx-auto" style="border: 1px solid silver">
+                    <h5 class="text-center text-primary" style="font-size: 15px">En toute sécurité</h5>
+                    <p style="color: grey;">Nous garantissons votre satisfaction du début à la fin de votre expérience de prof en ligne. Réservez sur un site 100% sécurisé en toute confiance !</p>
                 </div>
             </div>
+            <div class="row">
+            <!-- LOCALISATION -->
+            <div class="col-sm-4 marge-top-bottom">
+                <h4>Paramètres de localisation</h4>
+                <hr>
+            </div>
+            <!-- PRENDRE -->
+            <div class="col-sm-4 marge-top-bottom">
+                <h4>Prendre des cours</h4>
+                <hr>
+                <a href="">Comment ça marche ?</a><br>
+                <a href="">Pourquoi prendre un cours ?</a><br>
+                <a href="">La garantie UltraProf</a><br>
+                <a href="">FAQ éléve</a><br>
+            </div>
+            <!-- COURS -->
+            <div class="col-sm-4 marge-top-bottom">
+                <h4>Donner des cours</h4>
+                <hr>
+                <a href="">Devenir un ultra prof</a><br>
+                <a href="">Créer son ultra annonce d'ultra prof</a><br>
+                <a href="">L'ultra guide de l'ultra prof</a><br>
+                <a href="">FAQ prof</a><br>
+            </div>
+        </div>
         </div>
     </footer>
 
