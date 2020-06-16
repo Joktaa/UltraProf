f3bcb49683a9e7cd2b90e3ab5cd3456f760fab23
Inebhis <inebhis@gmail.com>
2020-06-16 23:51:07 +0200

    jremet la bdd localhost (pb mise en ligne)

--- class/UtilisateurDB.php
+++ class/UtilisateurDB.php
@@ -9,7 +9,7 @@ class UtilisateurDB{
 
 		try
 		{			
-		    $connexion = new PDO('mysql:host=sql301.epizy.com;dbname=epiz_26027138_ultraprof;charset=utf8', 'epiz_26027138', 'P9pMqykWRZ3Ef',array(
+		    $connexion = new PDO('mysql:host=localhost;dbname=ultraprof;charset=utf8', 'root', '',array(
 				        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 				        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 				    )
--- inc/bdd.php
+++ inc/bdd.php
@@ -1,7 +1,7 @@
 <?php
 try
 {			
-    $connexion = new PDO('mysql:host=sql301.epizy.com;dbname=epiz_26027138_ultraprof;charset=utf8', 'epiz_26027138', 'P9pMqykWRZ3Ef',array(
+    $connexion = new PDO('mysql:host=localhost;dbname=ultraprof;charset=utf8', 'root', '',array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     )
